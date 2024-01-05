ad128b8a6c800985b75d0fee66a56c809735c637
SH-Kirill <42139135+SH-Kirill@users.noreply.github.com>
2024-01-05 18:08:29 +0400

    Add files via upload

---
+++ CS/DiagramNestedItemsExample/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using DevExpress.Xpf.Diagram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DiagramNestedItemsExample {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+        int i = 999;
+        private void DiagramDataBindingBehavior_DiagramItemAdding(object sender, DevExpress.Xpf.Diagram.DiagramDiagramItemAddingEventArgs e) {
+            if (e.Item is DiagramShape shape) {
+                e.DataItem = new Employee() { EmployeeID = i, EmployeeName = "New Emp: " + i };
+                e.Item.SetBinding(DiagramShape.ContentProperty, new Binding("EmployeeName"));
+                e.Item.Background = Brushes.Orange;
+                i++;
+            }
+        }
+    }
+}
---
+++ CS/DiagramNestedItemsExample/Selectors.cs
@@ -0,0 +1,53 @@
+﻿using DevExpress.Diagram.Core;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DiagramNestedItemsExample {
+
+    public class ItemsSelector : IChildrenSelector {
+        public IEnumerable<object> GetChildren(object parent) {
+            if (parent is Department)
+                return ((Department)parent).Sections;
+            else if (parent is Section)
+                return ((Section)parent).Employees;
+            return null;
+        }
+    }
+    public class KeySelector : IKeySelector {
+        object IKeySelector.GetKey(object obj) {
+            if (obj is Department)
+                return ((Department)obj).DepartmentID;
+            else if (obj is Section)
+                return ((Section)obj).SectionID;
+            else if (obj is Employee)
+                return ((Employee)obj).EmployeeID;
+            return obj;
+        }
+    }
+
+    public class DiagramItemTemplateSelector : DataTemplateSelector {
+        public DataTemplate DepartmentTemplate {
+            get;
+            set;
+        }
+        public DataTemplate SectionTemplate
+        {
+            get;
+            set;
+        }
+        public DataTemplate EmployeeTemplate {
+            get;
+            set;
+        }
+        public override DataTemplate SelectTemplate(object item, DependencyObject container) {
+            if (item is Department)
+                return DepartmentTemplate;
+            else if (item is Section)
+                return SectionTemplate;
+            else if (item is Employee)
+                return EmployeeTemplate;
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
